8fe5c9d8f64f0b5ab6c1bd730464abc7abb6a98c
Fizoo <oleg.mykytchuk@gmail.com>
2022-05-20 13:06:02 +0300

    final update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,9 @@ import {HTTP_INTERCEPTORS} from "@angular/common/http";
 import {AuthInterceptor} from "./shared/auth.interceptor";
 import {IdxColorDirective} from "./shared/components/post/idx-color.directive";
 
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
 
 
 
@@ -37,7 +40,13 @@ const  INTERCEPTOR_PROVIDERS:Provider={
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
 
   ],
   providers: [INTERCEPTOR_PROVIDERS],
